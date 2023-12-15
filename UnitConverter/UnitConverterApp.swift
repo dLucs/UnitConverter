ddcd1f8aa7ca94bd4b4e0f013d807ba6bced8e31
dLucs <lucas_61@hotmail.es>
2023-12-15 12:10:47 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Lucas on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
