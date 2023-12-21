ff7c4bda6304658d9531e55aaabf616e6283e97a
dLucs <lucas_61@hotmail.es>
2023-12-21 13:10:45 +0100

    added all the logic to do the conversions

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -15,20 +15,79 @@ struct ContentView: View {
     @FocusState private var inputIsFocused: Bool
     
     let conversions = ["Length", "Temperature", "Time"]
-    let lengths = ["meters", "kilometers", "feet", "yards", "miles"]
+    let lengths = ["meters", "kilometers","centimeters", "millimeters", "inches","feet", "yards", "miles"]
     let temperatures = ["Celsius", "Fahrenheit", "Kelvin"]
-    let times = ["seconds", "minutes", "hours", "days"]
+    let times = ["seconds", "minutes", "hours", "days", "weeks", "months", "years"]
     
-  
+    func convertLength() -> Double {
+         let conversionFactors: [Double] = [
+             1.0, // meters
+             1000.0, // kilometers
+             0.01, // centimeters
+             0.001, // millimeters
+             0.0254, // inches
+             0.3048, // feet
+             0.9144, // yards
+             1609.34 // miles
+         ]
+
+        let fromFactor = conversionFactors[lengths.firstIndex(of: inputUnit) ?? 0]
+
+         let toFactor = conversionFactors[lengths.firstIndex(of: outputUnit) ?? 0]
+
+         let result = inputNumber * (fromFactor / toFactor)
+         return result
+     }
+    
+    func convertTemperature() -> Double {
+        let conversionFormulas: [(Double) -> Double] = [
+                { $0 },                             // Celsius to Celsius
+                { ($0 * 9/5) + 32 },                 // Celsius to Fahrenheit
+                { $0 + 273.15 }                      // Celsius to Kelvin
+            ]
+
+            let result = conversionFormulas[temperatures.firstIndex(of: inputUnit) ?? 0](inputNumber)
+            let convertedResult = conversionFormulas[temperatures.firstIndex(of: outputUnit) ?? 0](result)
+
+            return convertedResult
+        }
+    
+    func convertTime() -> Double {
+         let conversionFactors: [Double] = [
+            1.0,      // seconds
+            60.0,     // minutes
+            3600.0,   // hours
+            86400.0,  // days
+            604800.0, // weeks
+            2628000.0, // average days in a month (30.44 days)
+            31536000.0 // average days in a year (365.25 days)
+             
+         ]
+
+        let fromFactor = conversionFactors[times.firstIndex(of: inputUnit) ?? 0]
+
+         let toFactor = conversionFactors[times.firstIndex(of: outputUnit) ?? 0]
+
+         let result = inputNumber * (fromFactor / toFactor)
+         return result
+     }
+
     
     
     var conversionResult: Double {
        
-            
-            var result: Double = 0.0
-            
-           
-            return result
+       
+        switch conversionType {
+          case "Length":
+            return convertLength()
+          case "Temperature":
+            return convertTemperature()
+          case "Time":
+            return convertTime()
+          default:
+              return 0.0
+          }
+     
         }
 
     
